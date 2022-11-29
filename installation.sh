60b617acccaf5015158541a84325f27d415c242b
Radovan Blaha <kedar.blaha@gmail.com>
2022-11-29 23:33:29 +0100

    updated packages

--- installation.sh
+++ installation.sh
@@ -11,10 +11,13 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
+#will not install things need to fix
 paru --needed -Sy - < pkglist.txt || error "Failed to install aur packages"
 
 # todo add steam to aurlist and somehow add preselected options
 # Create .config if not present
 [ ! -d ~/.config ] && mkdir ~/.config
-[ ! -d ~/.config ] && mkdir ~/Computer
 # todo add auto clone for dotfiles
+
+# add sddm activation
+#todo add copy config
